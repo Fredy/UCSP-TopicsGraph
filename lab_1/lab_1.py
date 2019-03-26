f2d3e12e9c8b8ca6a8240c2be07c324702cd6a62
Fredy Álvarez <fre.alvarezb@gmail.com>
2019-03-26 18:09:02 -0500

    Lab1: completed

--- lab_1/lab_1.py
+++ lab_1/lab_1.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import numpy as np
 from math import (cos, sin, radians, floor, ceil, exp, log)
@@ -17,7 +18,7 @@ def resize(src, xfactor, yfactor):
 
     for i in range(shape[0]):
         for j in range(shape[1]):
-            out[i,j] = src[floor(i // yfactor), floor(j // xfactor)]
+            out[i, j] = src[floor(i // yfactor), floor(j // xfactor)]
 
     return out
 
@@ -29,62 +30,71 @@ def rotate(src, angle, pivot=None):
     shape = src.shape
     out = np.zeros(shape, src.dtype)
 
-    if pivot == None:
-        pivot = (shape[1] // 2, shape[0] // 2)
-    print(pivot)
+    if pivot is None:
+        pivot = (shape[0] // 2, shape[1] // 2)
 
-    for r in range(shape[0]):
-        for c in range(shape[1]):
-            rc, rr = _rotate_point((c, r), pivot, angle)
-            rc = round(rc)
-            rr = round(rr)
-            if (rr >= shape[0] or rr < 0 or
-                    rc >= shape[1] or rc < 0):
-                continue
-            out[rr, rc] = src[r, c]
+    for i in range(shape[0]):  # rows
+        for j in range(shape[1]):  # cols
+            x, y = _rotate_pos((shape[1] - j - 1 , i), pivot, angle)
+            x = shape[1] - x - 1
 
+            x = round(x)
+            y = round(y)
+
+            if 0 <= x < shape[1] and 0 <= y < shape[1]:
+                out[y,x] = src[i, j]
+        
     return out
 
 
-def _rotate_point(point, center, angle):
-    """Angle in degrees"""
+def _rotate_pos(point, center, angle):
+    """:params angle: in degrees"""
+    angle = radians(angle)
     x, y = point
     cx, cy = center
-    angle = radians(angle)
     _cos = cos(angle)
     _sin = sin(angle)
 
-    x = (x - cx) * _cos - (y - cy) * _sin + cx
-    y = (x - cx) * _sin + (y - cy) * _cos + cy
+    x -= cx
+    y -= cy
 
-    return (abs(x), abs(y))
+    nx = x * _cos - y * _sin
+    ny = x * _sin + y * _cos
 
-@np.vectorize
+    return nx + cx, ny + cy
+
+
 def exp_filter(x):
-    return pow(2, exp) % 255
+    return pow(x, 2.5) % 255
+
 
 @np.vectorize
 def log_filter(x):
     if x == 0:
         return 0
-    return log(x)
+    x /= 255
+    return (log(x) * 255) % 255
 
 @np.vectorize
 def inverse_filter(x):
     return 255 - x
 
+
 if __name__ == "__main__":
     img = cv2.imread('../imgs/woman.jpg')
+
+    os.makedirs('res', exist_ok=True)
+
     positive = translation(img, 50, 40)
     negative = translation(img, -50, -40)
-    cv2.imwrite('translation_positive.jpg', positive)
-    cv2.imwrite('translation_negative.jpg', negative)
+    cv2.imwrite('res/translation_positive.jpg', positive)
+    cv2.imwrite('res/translation_negative.jpg', negative)
 
-    cv2.imwrite('rotate.jpg', rotate(img, 90))
+    cv2.imwrite('res/rotate.jpg', rotate(img, 15))
 
-    cv2.imwrite('upscale.jpg', resize(img, 3,3))
-    cv2.imwrite('downscale.jpg', resize(img, 0.4,0.4))
+    cv2.imwrite('res/upscale.jpg', resize(img, 3, 3))
+    cv2.imwrite('res/downscale.jpg', resize(img, 0.4, 0.4))
 
-    cv2.imwrite('exp.jpg', exp_filter(img))
-    cv2.imwrite('log.jpg', log_filter(img))
-    cv2.imwrite('inverse.jpg', inverse_filter(img))
+    cv2.imwrite('res/exp.jpg', exp_filter(img))
+    cv2.imwrite('res/log.jpg', log_filter(img))
+    cv2.imwrite('res/inverse.jpg', inverse_filter(img))
