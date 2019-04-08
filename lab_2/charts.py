e45be5c4e696e06feaa1a78f54fa8c5eb793a36d
Fredy Álvarez <fre.alvarezb@gmail.com>
2019-04-08 01:31:48 -0500

    Lab2: finished

---
+++ lab_2/charts.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def draw_comp_hist(src, out, file_name):
+    fig, (ax0, ax1) = plt.subplots(ncols=2, figsize=(8, 4))
+
+    ax0.hist(src.flatten(), np.arange(257), density=True, histtype='stepfilled', color='g', alpha=0.75)
+    ax0.set_title('Original')
+
+    ax1.hist(out.flatten(), np.arange(257), density=True, histtype='stepfilled', color='b', alpha=0.75)
+    ax1.set_title('Equalized')
+
+    fig.tight_layout()
+    plt.savefig(file_name)
--- lab_2/lab_2.py
+++ lab_2/lab_2.py
@@ -2,6 +2,9 @@ import os
 import cv2
 import numpy as np
 from math import exp, sqrt
+import sys
+
+from charts import draw_comp_hist
 
 
 def _def_operation(window, *args, **kwargs):
@@ -41,32 +44,67 @@ def min_operation(window):
 def max_operation(window):
     return window.max()
 
+
 def gaussian_kernel_1D(size, stdev):
     filter = []
-    sqr = stdev ** 2
+
+    if stdev <= 0:
+        stdev = ((size-1)*0.5 - 1)*0.3 + 0.8
+
+    sqr = 2 * stdev ** 2
     i = (size - 1) // 2
+    sum = 0
     for x in range(-i, i + 1):
-        tmp = 1 / sqrt(2 * np.pi * sqr)
-        tmp = tmp * exp(- (x ** 2)/ (2 * sqr))
+        tmp = exp(- (x ** 2) / sqr)
+        sum += tmp
         filter.append(tmp)
 
-    return np.array(filter)
+    sum = 1/sum
+    return np.array(filter) * sum
 
-def gauss_filter(src, size, stdev):
-    ifilter = gaussian_kernel_1D(size, stdev)
-    def operation(window):
-        shape = window.shape
-        if shape[1] < size: # * column
-            out = window * ifilter[:shape[1]]
-        else:
-            out = window * ifilter
-        if shape[0] < size:
-            ifilter_T = ifilter[:shape[0],np.newaxis] 
-        else:
-            ifilter_T = ifilter[:,np.newaxis] 
-        out *= ifilter_T # * row
-        return out.sum()
-    return basic_filter(src, size, operation)
+
+def gauss_filter(src, window_size, stdev):
+    out = np.empty(src.shape, src.dtype)
+
+    rows, cols = img.shape[:2]
+    step = (window_size - 1) // 2
+    filter1D = gaussian_kernel_1D(window_size, stdev)
+
+    for i in range(rows):
+        # in intervals everything that is less or equals to -rows == 0
+        # and everything that is greater or equals to rows == rows
+        r0 = i - step - rows
+        r1 = i + step + 1
+
+        rleft = 0
+        rright = rows
+        if -rows > r0:
+            rleft = - rows - r0
+        if rows < r1:
+            rright = rows - r1
+
+        vfilter = filter1D[rleft: rright, np.newaxis]
+
+        for j in range(cols):
+            c0 = j - step - cols
+            c1 = j + step + 1
+
+            cleft = 0
+            cright = cols
+            if -cols > c0:
+                cleft = - cols - c0
+            if cols < c1:
+                cright = cols - c1
+
+            hfilter = filter1D[cleft: cright]
+
+            window = src[r0:r1, c0:c1]
+
+            tmp = window * vfilter * hfilter
+            out[i, j] = tmp.sum()
+
+    return out
+
 
 def gen_weighted_filter(size):
     filter = np.zeros((size, size))
@@ -96,15 +134,59 @@ def _gen_weighted_filter(filter, weight, idx, max_idx):
         _gen_weighted_filter(filter, weight, (r, c + 1), max_idx)
 
 
+def histogram_expansion(img, min=0, max=255):
+    min_value = img.min()
+    max_value = img.max()
+
+    tmp = (max - min) / (max_value - min_value)
+    out = (img - min_value) * tmp + min
+
+    return out
+
+
+def histogram_equalization(img, colors=256):
+    out = np.empty(img.shape, img.dtype)
+
+    hist = np.histogram(img, bins=np.arange(colors + 1), density=True)[0]
+    hist = hist.cumsum() * (colors - 1)
+    new_values = [round(i) for i in hist]
+    print(len(new_values))
+
+    rows, cols = out.shape[:2]
+    for i in range(rows):
+        for j in range(cols):
+            # print(img[i,j])
+            out[i,j] = new_values[img[i,j]]
+
+    return out
+
+
 if __name__ == "__main__":
-    img = cv2.imread('../imgs/2/lenaG.png', 0)
+    img_name = '../imgs/2/lenaS.jpg'
+    if len(sys.argv) > 1:
+        img_name = sys.argv[1]
+    img = cv2.imread(img_name, 0)
+
+    if img is None:
+        print('Can not read `{}`'.format(img_name))
+        exit(-1)
 
     os.makedirs('res', exist_ok=True)
 
-    cv2.imwrite('res/filter_1.jpg', basic_filter(img, 3))
+    # cv2.imwrite('res/filter_1.jpg', basic_filter(img, 9))
 
-    cv2.imwrite('res/median.jpg', basic_filter(img, 9, median_operation))
-    cv2.imwrite('res/min.jpg', basic_filter(img, 9, min_operation))
-    cv2.imwrite('res/max.jpg', basic_filter(img, 9, max_operation))
+    # cv2.imwrite('res/median.jpg', basic_filter(img, 9, median_operation))
+    # cv2.imwrite('res/min.jpg', basic_filter(img, 9, min_operation))
+    # cv2.imwrite('res/max.jpg', basic_filter(img, 9, max_operation))
 
-    cv2.imwrite('res/gauss.jpg', gauss_filter(img, 9, 1))
+    # gauss_res = gauss_filter(img, 5, 0)
+    # cv2.imwrite('res/gauss.jpg', np.append(img, gauss_res, axis=1))
+
+    expansion = histogram_expansion(img)
+    cv2.imwrite('res/hist_expansion.jpg', expansion)
+
+    draw_comp_hist(img, expansion, 'res/hist_expansion_chart.svg')
+
+    equalization = histogram_equalization(img)
+    cv2.imwrite('res/hist_equalization.jpg', equalization)
+    draw_comp_hist(img, equalization, 'res/hist_equalization_chart.svg')
